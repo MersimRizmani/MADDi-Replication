4a47798fae1562b4f1b8a18573ec9a08a3683b1b
araghu9 <54913635+araghu9@users.noreply.github.com>
2024-04-19 22:33:29 -0500

    updated concat.py

--- preprocess_genetic/concat_vcfs.py
+++ preprocess_genetic/concat_vcfs.py
@@ -9,7 +9,7 @@ def main():
     
     
     files = os.listdir("YOUR_PATH_TO_FILTERED_VCFS")
-    diag = pd.read_csv("YOUR_PATH_TO_DIAGNOSIS_TABLE")[["index", "Group"]]
+    diag = pd.read_csv("YOUR_PATH_TO_DIAGNOSIS_TABLE")[["Subject", "GROUP"]].rename(columns={"Subject": "index", "GROUP":"Group"})
     
     vcfs = []
     
